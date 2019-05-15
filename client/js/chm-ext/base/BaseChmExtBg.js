7566902e9508c825bea6e9c19608430242288074
mac <admin666>
2019-05-16 00:41:50 +0800

    test

--- client/js/chm-ext/base/BaseChmExtBg.js
+++ client/js/chm-ext/base/BaseChmExtBg.js
@@ -321,11 +321,11 @@ class BaseChmExtBg {
                                 if ('Network.requestWillBeSent' == method && 'request' in params && 'url' in params.request && params.request.url) {
                                     let urlMatchRet=params.request.url.match(requestMatchReg);
                                     urlMatchRet ? targetRequestIds[params.requestId] = params.requestId : false;
-                                    //urlMatchRet && this.logToCurSender('requestWillBeSent:'+params.request.url+',isMatch:'+urlMatchRet[0]+',requestId:'+params.requestId);
+                                    urlMatchRet && this.logToCurSender('requestWillBeSent:'+params.request.url+',isMatch:'+urlMatchRet[0]+',requestId:'+params.requestId);
                                 }
                                 //网络加载完成后再去获取数据，否则可能抓取的数据不全(丢失部分)
                                 if ('Network.loadingFinished' == method && params.requestId in targetRequestIds) {
-                                    //this.logToCurSender('loadingFinished:'+JSON.stringify(params)+',requestId:'+params.requestId);
+                                    this.logToCurSender('loadingFinished:'+JSON.stringify(params)+',requestId:'+params.requestId);
                                     chrome.debugger.sendCommand({
                                         tabId: tmpTabId
                                     }, 'Network.getResponseBody', {
@@ -341,7 +341,7 @@ class BaseChmExtBg {
                                                 //alert('dstUrl:' + dstUrl + ',hit:' + responseMatchReg + ',findRet:' + JSON.stringify(findRet) + ',response_body:' + response.body);
                                                 'function' === typeof cbFunc ? cbFunc(dstUrl,findRet[0]) : alert(findRet[0]);
                                                 //alert('dstUrl:'+dstUrl+',hit:'+responseMatchReg+',findRet:'+findRet[0]);
-                                                //this.logToCurSender(findRet[0]);
+                                                this.logToCurSender(findRet[0]);
                                                 //以防url变化，通过tabid解绑
                                                 if (autoDetach) {
                                                     this.disableNetworkMonitorByTabId(tmpTabId);
